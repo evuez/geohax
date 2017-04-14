34b43e2e2fedad11d80e13381ccc5879a0b6c382
Bartlomiej Rozanski <ebartlomiej@gmail.com>
2017-04-14 13:32:13 +0200

    Calculate all geohashes within an envelope

--- test/geohax_test.exs
+++ test/geohax_test.exs
@@ -13,4 +13,14 @@ defmodule GeohaxTest do
   test "should find geohash neighbors" do
     assert Geohax.neighbors("311x1r") == [north: "311x32", south: "311x1q", east: "311x1x", west: "311x1p"]
   end
+
+  test "should return all the geohashes within an envelope" do
+    assert Geohax.within([52.291725, 16.731831, 52.508736, 17.071703]) ==
+      ["u37ck", "u37cm", "u37cq", "u37cr", "u3k12", "u3k13", "u3k16", "u3k17", "u3k1k",
+       "u37cs", "u37ct", "u37cw", "u37cx", "u3k18", "u3k19", "u3k1d", "u3k1e", "u3k1s",
+       "u37cu", "u37cv", "u37cy", "u37cz", "u3k1b", "u3k1c", "u3k1f", "u3k1g", "u3k1u",
+       "u37fh", "u37fj", "u37fn", "u37fp", "u3k40", "u3k41", "u3k44", "u3k45", "u3k4h",
+       "u37fk", "u37fm", "u37fq", "u37fr", "u3k42", "u3k43", "u3k46", "u3k47", "u3k4k",
+       "u37fs", "u37ft", "u37fw", "u37fx", "u3k48", "u3k49", "u3k4d", "u3k4e", "u3k4s"]
+  end
 end
